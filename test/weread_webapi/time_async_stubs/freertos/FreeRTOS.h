57c07eb0086970a13e63a5424933df4ed7db5bef
ar0c <anchor_lee@163.com>
2026-09-25 00:42:34 +0800

    fix(weread): keep time worker portable in simulator

--- test/weread_webapi/time_async_stubs/freertos/FreeRTOS.h
+++ test/weread_webapi/time_async_stubs/freertos/FreeRTOS.h
@@ -7,4 +7,5 @@ using portMUX_TYPE = std::mutex;
 #define taskENTER_CRITICAL(p) (p)->lock()
 #define taskEXIT_CRITICAL(p) (p)->unlock()
 #define pdMS_TO_TICKS(v) (v)
+constexpr int pdPASS = 1;
 constexpr int pdTRUE = 1;
